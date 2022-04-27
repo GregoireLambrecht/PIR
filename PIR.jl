6190d3122704cf7111a8f90f7715d3d582b3303c
Gregoire LAMBRECHT <gregoire.lambrecht@eleves.enpc.fr>
2022-04-27 15:19:05 +0200

    commit1

--- PIR.jl
+++ PIR.jl
@@ -46,7 +46,7 @@ function TraceT(n)
 	(T,E./eV)
 end
 
-function doubleBarrT(n,ϵ,V1,V2,L1,L2,Lp)
+function ParamDB(ϵ,V1,V2,L1,L2,Lp)
 	Bar1 = init_Barr(ϵ,L1,V1)
 	Bar2 = init_Barr(ϵ,L2,V2)
 	N = Bar1.N * Bar2.N
@@ -55,32 +55,39 @@ function doubleBarrT(n,ϵ,V1,V2,L1,L2,Lp)
 	r = N[1,2]*t
 	A1 = 1 + r
 	B1 =1im*k*(1-r)/Bar1.K
-	α = -(1im*exp(-1im*k*L1)/2*k)*((1im*k*A1 + B1*Bar1.K)*cosh(L1*Bar1.K) + (1im*k*B1 + A1*Bar1.K)*sinh(L1*Bar1.K))
-	β = (1im*exp(1im*k*L1)/2*k)*((-1im*k*A1 + B1*Bar1.K)*cosh(L1*Bar1.K) + (-1im*k*B1 + A1*Bar1.K)*sinh(L1*Bar1.K))
+	α = -(1im*exp(-1im*k*L1)/(2*k))*((1im*k*A1 + B1*Bar1.K)*cosh(L1*Bar1.K) + (1im*k*B1 + A1*Bar1.K)*sinh(L1*Bar1.K))
+	β = (1im*exp(1im*k*L1)/(2*k))*((-1im*k*A1 + B1*Bar1.K)*cosh(L1*Bar1.K) + (-1im*k*B1 + A1*Bar1.K)*sinh(L1*Bar1.K))
 	L = L1 + L2 + Lp
 	A2 = (exp(1im*k*L)*t/Bar2.K)*(cosh(L*Bar2.K)*Bar2.K - 1im*k*sinh(L*Bar2.K))
 	B2 = (exp(1im*k*L)*t/Bar2.K)*(cosh(L*Bar2.K)*1im*k - Bar2.K*sinh(L*Bar2.K))
-	z = [i/n for i=1:n]
-	dl = 2*L/n
-	ψ = zeros(ComplexF64,n)
-	for i = 1:Int64(n/4)
-		ψ[i] = exp(1im*k*2L*z[i]) + r*exp(-1im*k*2L*z[i])
-	end
-	for i = Int64(n/4):Int64(round(((L/2)+L1)/dl))
-		ψ[i] = A1*cosh(Bar1.K*2L*z[i]) + B1*sinh(Bar1.K*2L*z[i])
-	end
-	for i = Int64(round(((L/2)+L1)/dl)):Int64(round(((L/2)+L1 + Lp)/dl))
-		ψ[i] = α*exp(1im*k*2L*z[i]) + β*exp(-1im*k*2L*z[i])
-	end
-	for i = Int64(round(((L/2)+L1 + Lp)/dl)):Int64(round(((L/2)+L)/dl))
-		ψ[i] = A2*cosh(Bar1.K*2L*z[i]) + B2*sinh(Bar1.K*2L*z[i])
-	end
-	for i = Int64(round(((L/2)+L)/dl)):n
-		ψ[i] = t*exp(1im*k*2L*z[i])
-	end
-
-	pl = plot(z,abs2.(ψ))
-	savefig(pl,"solution.pdf")
+	(t,r,A1,B1,α,β,A2,B2)
 end
 
-doubleBarrT(100,0.3*eV,eV,eV,nm,nm,nm)
+function doubleBarrT(n)
+	E = [1/n*eV*i for i=1:(n-1)]
+	T = zeros(n-1)
+	#Premiere situation
+	for k=1:(n-1)
+		(t,r,A1,B1,α,β,A2,B2) = ParamDB(E[k],eV,eV,nm,nm,nm)
+		T[k] = real.(t't)
+	end
+	pl1 = plot(E./eV,T,xlabel = "Energie en eV",ylabel = "T(E)", title = "T(E) avec L1 = L2 = Lp = 1 nm et V1 = V2 = 1eV")
+	savefig(pl1,"situation1.pdf")
+	#Situation 2 V1 = 2 V2
+	for k=1:(n-1)
+		(t,r,A1,B1,α,β,A2,B2) = ParamDB(E[k],2*eV,eV,nm,nm,nm)
+		T[k] = real.(t't)
+	end
+	pl1 = plot(E./eV,T,xlabel = "Energie en eV",ylabel = "T(E)", title = "T(E) avec L1 = L2 = Lp = 1 nm et V1 = 2V2 = 2eV")
+	savefig(pl1,"situation2.pdf")
+	#Situation 3, V2 varie, ϵ = 0.4 eV
+	V = [(eV/2) *(1 + 2*i/n) for i = 1:(n-1)]
+	for k=1:(n-1)
+		(t,r,A1,B1,α,β,A2,B2) = ParamDB(0.4 *eV,eV,V[k],nm,nm,nm)
+		T[k] = real.(t't)
+	end
+	pl1 = plot(V./eV,T,xlabel = "V2 en eV",ylabel = "T(V2)", title = "T(V2), L1 = L2 = Lp = 1 nm, V1 = 1eV, E = 0.4 eV")
+	savefig(pl1,"situation3.pdf")
+end
+
+doubleBarrT(100)
