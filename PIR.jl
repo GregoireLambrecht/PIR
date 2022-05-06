fc10f55337c32fa3b53f114d700a5cb33e3dcbb3
Gregoire LAMBRECHT <gregoire.lambrecht@eleves.enpc.fr>
2022-05-06 11:46:10 +0200

    mi-hauteur

--- PIR.jl
+++ PIR.jl
@@ -1,10 +1,14 @@
-using "tools.jl"
+include("tools.jl")
 using Plots
 
 const ħ=6.63e-34
-const m = 9.1e-31
-const eV = 1.6e-19
-const nm = 1e-9
+const m = 9.1e-32  #Un dixième de la masse de l'éléctron libre
+const eV = 1.6e-19 #Un électron Volt
+const nm = 1e-9 #Le nanométre
+
+#La hauteur des potentiels dans les composants sont généralement
+#Entre 0.2 et 0.5 eV
+#Les largeurs barrières et puits sont environ de 2 à 5 nm
 
 mutable struct Barr
 	L
@@ -65,32 +69,37 @@ function ParamDB(ϵ,V1,V2,L1,L2,Lp)
 end
 
 function doubleBarrT(n)
-	E = [1/n*eV*i for i=1:(n-1)]
+	V = 0.4
+	l = 3
+	E = [1/n*V*eV*i for i=1:(n-1)]
 	T = zeros(n-1)
 	#Premiere situation
 	for k=1:(n-1)
-		(t,r,A1,B1,α,β,A2,B2) = ParamDB(E[k],eV,eV,nm,nm,nm)
+		(t,r,A1,B1,α,β,A2,B2) = ParamDB(E[k],V*eV,V*eV,l*nm,l*nm,l*nm)
 		T[k] = real.(t't)
 	end
-	pl1 = plot(E./eV,T,xlabel = "Energie en eV",ylabel = "T(E)", title = "T(E) avec L1 = L2 = Lp = 1 nm et V1 = V2 = 1eV")
+	ϵres = argmax(T) * V/n #L'energie de resonnance en eV
+	ϵres = (round(ϵres * 10000))/10000 #On arrondie
+	Γ = Largeur(T,0.001)/n #La largeur à mi-hauteur
+	pl1 = plot(E./eV,T,xlabel = "Energie en eV, ϵres = $ϵres eV, Γ = $Γ eV" ,ylabel = "T(E)", title = "T(E) avec L1 = L2 = Lp = $l nm et V1 = V2 = $V eV")
 	savefig(pl1,"situation1.pdf")
 	#Situation 2 V1 = 2 V2
 	for k=1:(n-1)
-		(t,r,A1,B1,α,β,A2,B2) = ParamDB(E[k],2*eV,eV,nm,nm,nm)
+		(t,r,A1,B1,α,β,A2,B2) = ParamDB(E[k],2*V*eV,V*eV,l*nm,l*nm,l*nm)
 		T[k] = real.(t't)
 	end
-	pl1 = plot(E./eV,T,xlabel = "Energie en eV",ylabel = "T(E)", title = "T(E) avec L1 = L2 = Lp = 1 nm et V1 = 2V2 = 2eV")
+	pl1 = plot(E./eV,T,xlabel = "Energie en eV",ylabel = "T(E)", title = "T(E) avec L1 = L2 = Lp = $l nm et V2 = V1/2 = $V eV")
 	savefig(pl1,"situation2.pdf")
 	#Situation 3, V2 varie, ϵ = 0.4 eV
-	V = [(eV/2) *(1 + 2*i/n) for i = 1:(n-1)]
+	V2 = [(eV*V/2) *(1 + 2*i/n) for i = 1:(n-1)]
 	for k=1:(n-1)
-		(t,r,A1,B1,α,β,A2,B2) = ParamDB(0.4 *eV,eV,V[k],nm,nm,nm)
+		(t,r,A1,B1,α,β,A2,B2) = ParamDB(ϵres*eV,V*eV,V2[k],l*nm,l*nm,l*nm)
 		T[k] = real.(t't)
 	end
-	pl1 = plot(V./eV,T,xlabel = "V2 en eV",ylabel = "T(V2)", title = "T(V2), L1 = L2 = Lp = 1 nm, V1 = 1eV, E = 0.4 eV")
+	pl1 = plot(V2./eV,T,xlabel = "V2 en eV",ylabel = "T(V2)", title = "T(V2), L1=L2=Lp=$l nm, V1=$V eV, E=$ϵres eV")
 	savefig(pl1,"situation3.pdf")
 end
 
 
 
-doubleBarrT(1000)
+doubleBarrT(10000)
